8965efed5cf7d3236814423dbea5ea934a989172
Sameera1920 <sameera1920@gmail.com>
2023-04-24 17:07:10 +0530

    remove logo at navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -36,7 +36,7 @@ const NavBar = () => {
     <div className="nav-container">
       <Navbar color="light" light expand="md">
         <Container>
-          <NavbarBrand className="logo" />
+          {/* <NavbarBrand className="logo" /> */}
           <NavbarToggler onClick={toggle} />
           <Collapse isOpen={isOpen} navbar>
             <Nav className="mr-auto" navbar>
