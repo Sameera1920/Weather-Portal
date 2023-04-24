21bbf2cf967ae9578b50a47f74b085847c2644d1
Sameera1920 <sameera1920@gmail.com>
2023-04-24 10:31:38 +0530

    renamed as WeatherApp.css and WeatherApp.js

--- src/src_weather_app/index.js
+++ src/src_weather_app/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App9999 from "./App9999";
+import WeatherApp from "./WeatherApp";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-  <App9999 />
+  <WeatherApp />
   // </React.StrictMode>
 );
 
