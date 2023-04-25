5777c26d268e13321d6afb630cb5b49afe5a6570
Sameera1920 <sameera1920@gmail.com>
2023-04-25 11:21:55 +0530

    Hero component deleted and embeded to Home.js

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,14 +1,18 @@
-import React, { Fragment } from "react";
+import React from "react";
+// import { useAuth0 } from "@auth0/auth0-react";
 
-import Hero from "../components/Hero";
-// import Content from "../components/Content";
-
+// import logo from "../src_weather_app/weather_app_assets/app_logo.png";
 const Home = () => (
-  <Fragment>
-    <Hero />
-    {/* <hr />
-    <Content /> */}
-  </Fragment>
+  <div className="text-center hero my-5">
+    {/* <img className="mb-3 app-logo" src={logo} alt="app logo" width="120" /> */}
+
+    <h2 className="mb-4">Please Login to View Weather Data</h2>
+
+    {/* <p className="lead">
+      This is a sample application that demonstrates an authentication flow for
+      an SPA, using <a href="https://reactjs.org">React.js</a>
+    </p> */}
+  </div>
 );
 
 export default Home;
