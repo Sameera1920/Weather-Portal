505606b942b5bc1bdfd05b170392ce5670463c95
Sameera1920 <sameera1920@gmail.com>
2023-04-27 12:29:58 +0530

    created Badge.css and add logo to login page

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,7 +1,12 @@
 import React from "react";
+import "../style/Badge.css";
 
 const Home = () => (
   <div className="home-page">
+    <div className="badge">
+      <p className="logo"></p>
+      <p className="name">Weather App</p>
+    </div>
     <h2 className="messages">You are Logged in!</h2>
     <h2 className="messages">Please Navigate Weather App to View Data</h2>
   </div>
--- src/views/LoginPrompt.js
+++ src/views/LoginPrompt.js
@@ -1,7 +1,12 @@
 import React from "react";
-import "../style/WeatherApp.css";
+import "../style/Badge.css";
+
 const LoginPrompt = () => (
   <div className="login-prompt-page">
+    <div className="badge">
+      <p className="logo"></p>
+      <p className="name">Weather App</p>
+    </div>
     <h2 className="messages">Please Login to View Weather Data</h2>
   </div>
 );
